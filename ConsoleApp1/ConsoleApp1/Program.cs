4f80075f0ce5827366bd6fa298c41124ed372e20
SandwichPresident <shb1713@rit.edu>
2025-11-23 18:58:03 -0500

    Added Comments :(

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,18 +4,22 @@
     {
         static void Main(string[] args)
         {
+            // Simple console game that tracks score per user
+
+            // Get user name
             System.Console.WriteLine("Enter your name: ");
             string name = System.Console.ReadLine();
 
+            // Validate name
             if (string.IsNullOrWhiteSpace(name))
             {
                 System.Console.WriteLine("Invalid name. Exiting.");
                 return;
             }
 
+            // Load existing score or start new
             string fileName = name + ".txt";
             int score = 0;
-
             if (System.IO.File.Exists(fileName))
             {
                 string text = System.IO.File.ReadAllText(fileName);
@@ -25,6 +29,7 @@
             System.Console.WriteLine("\nStarting score: " + score);
             System.Console.WriteLine("Press keys to gain points. Press ENTER to end.\n");
 
+            // Scoring loop
             while (true)
             {
                 var key = System.Console.ReadKey(true);
@@ -35,7 +40,7 @@
                 score++;
                 System.Console.WriteLine("Score: " + score);
             }
-
+            // Save final score
             System.IO.File.WriteAllText(fileName, score.ToString());
 
             System.Console.WriteLine("\nGame over! Final score saved to " + fileName);
